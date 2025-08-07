88fd16a5fdb9623c41f50d0a091c1f3450054609
Somu878 <somuu99@protonmail.com>
2025-08-07 21:57:07 +0530

    added allowances

--- contracts/_somu.sol
+++ contracts/_somu.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.2 <0.9.0;
 
-contract SOMU {
+contract FLIXCOIN {
     uint public  totalSupply;
     address owner;
+    mapping (address=>mapping(address=>uint)) public allowances;
     mapping(address=>uint) public  balances ;
     constructor(){
         owner=msg.sender;
@@ -22,9 +23,27 @@ contract SOMU {
         totalSupply-=amount;
         // balances[msg.sender]-=amount;
     }
+    function approve(address _spender, uint amount) public returns (bool){
+        allowances[msg.sender][_spender] = amount;
+        return true;
+    }
+    function transerFrom(address _from,address _to, uint amount) public{
+        uint allowance = allowances[_from][msg.sender];
+        require(allowance>=amount,"insufficient allowance");
+        uint balance = balances[_from];
+        require(balance>=amount);
+        balances[_from]-=amount;
+        balances[_to]+=amount;
+        allowances[_from][msg.sender] -=amount;
+    }
     function transfer(address to,uint amount) public{
         require(balances[msg.sender]>=amount,"insufficient balance"); 
         balances[msg.sender]-=amount;
         balances[to]+=amount;
+    }
+    function burn(uint amount) public {
+        require(balances[msg.sender]>=amount);
+        balances[msg.sender]-=amount;
+        totalSupply-=amount;
     }
 }
